0b531eb7c247ccc8fea6005b9402b16b58e8dbfa
MartinezAgullo <pablo.martinez.agullo@gmail.com>
2024-08-28 20:03:09 +0200

    but this is

--- HousePricesPrediction.R
+++ HousePricesPrediction.R
@@ -5,12 +5,9 @@ library(rpart)     # Library for building decision trees
 # Read the data and store data in a tibble
 iowa_data <- read_csv("input/train.csv") 
 
-# Make sure Condition1 is a factor & not a char
-iowa_data$Condition1 <- as.factor(iowa_data$Condition1)
-
-# Ensure ExterCond and HeatingQC include all possible levels
-iowa_data$ExterCond <- factor(iowa_data$ExterCond, levels = c("Ex", "Gd", "TA", "Fa", "Po"))
-iowa_data$HeatingQC <- factor(iowa_data$HeatingQC, levels = c("Ex", "Gd", "TA", "Fa", "Po"))
+# Make sure all necessary columns are treated as factors
+factor_cols <- c("Condition1", "ExterCond", "HeatingQC", "Electrical", "PoolQC", "MiscFeature") # Add all relevant factor columns
+iowa_data[factor_cols] <- lapply(iowa_data[factor_cols], as.factor)
 
 # Structure of the dataframe
 str(iowa_data)
@@ -30,17 +27,13 @@ sample_index <- sample(1:nrow(iowa_data), 0.7 * nrow(iowa_data))
 train_set <- iowa_data[sample_index, ]
 test_set <- iowa_data[-sample_index, ]
 
-# Ensure factor levels in test set match those in the training set
-test_set$ExterCond <- factor(test_set$ExterCond, levels = levels(train_set$ExterCond))
-test_set$HeatingQC <- factor(test_set$HeatingQC, levels = levels(train_set$HeatingQC))
-test_set$Electrical <- factor(test_set$Electrical, levels = levels(train_set$Electrical))
-
-# Handle any new levels in the test set by setting them to NA or an existing level
-test_set$Electrical <- factor(test_set$Electrical, levels = levels(train_set$Electrical))
+# Ensure factor levels in test set match those in the training set for all factors
+for(col in factor_cols) {
+  test_set[[col]] <- factor(test_set[[col]], levels = levels(train_set[[col]]))
+}
 
 # Fit a model to your training set
 model <- rpart(SalePrice ~ ., data = train_set, method = "anova") 
-# Analysis of variance ANOVA
 
 # Predict on the test set
 predictions <- predict(model, test_set)
